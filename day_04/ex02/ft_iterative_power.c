cfc9634d82ddf857470eb7eaf6f5055bb3cc3682
Macka23 <louainajdi@outlook.be>
2021-04-14 14:33:16 +0200

    Train_C

--- day_04/ex02/ft_iterative_power.c
+++ day_04/ex02/ft_iterative_power.c
@@ -2,16 +2,34 @@
 
 int		ft_iterative_power(int nb, int power)
 {
-	
+	int i;
+	int result;
+	int add;
+
+	i = 1;
+	add = nb;
+	if (power < 0)
+		return(0);
+	if (power == 0)
+		return(1);
+	if ( power == 2)
+		return(nb * nb);
+	while (i < power)
+	{
+		result = add * nb;
+		add = result;
+		i++;
+	}
+	return(add);
 }
 
-int		main()
+int		main(void)
 {
 	int nb;
 	int power;
 
-	nb = 3;
+	nb = 12;
 	power = 2;
-	printf("résultat = %d", ft_iterative_power(nb, power););
+	printf("résultat = %d\n", ft_iterative_power(nb, power));
 	return(0);
 }
--- day_04/ex03/ft_recursive_power.c
+++ day_04/ex03/ft_recursive_power.c
@@ -1,9 +1,22 @@
+#include <stdio.h>
+
 int ft_recursive_power(int nb, int power)
 {
-	
+	if (power < 0)
+		return(0);
+	if  (power == 0)
+		return(1);
+	else 
+		return(nb * ft_recursive_power(nb, power - 1));
 }
 
 int		main()
 {
+	int nb;
+	int power;
 
+	nb = 3;
+	power = 4;
+	printf("résultat = %d\n", ft_recursive_power(nb, power));
+	return(0);
 }
